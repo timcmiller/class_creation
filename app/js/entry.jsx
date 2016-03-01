4ff06e3e08c68dd9b8e0c4460356760d775e7f5b
timcmiller <timcmiller@live.com>
2016-02-29 22:41:35 -0800

    gulp is now bundleing javascript

---
+++ app/js/entry.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <section>
+    Hello world
+  </section>,
+  document.getElementById('content')
+);
--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
-app.get('*', function(req, res) {
-  res.sendFile(__dirname + '/build/index.html');
-});
+
+app.use(express.static(__dirname + '/build'));
 
 app.use(function(req, res) {
   res.status(404).send('could not find file');
 });
 
-var server = app.listen(port, function() {
+app.listen(port, function() {
   console.log('Server up on port ' + port + '.');
 });
